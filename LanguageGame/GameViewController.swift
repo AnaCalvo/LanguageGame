7c45d2f01c13a90788012a21e6c99e5bd9430931
Ana Calvo <anacber@gmail.com>
2018-10-17 19:24:20 +0200

    Implement check of matching words and show next word

--- LanguageGame/GameViewController.swift
+++ LanguageGame/GameViewController.swift
@@ -25,6 +25,8 @@ class GameViewController: UIViewController {
     
     var vocabularyEn: [String] = []
     var vocabularySpa: [String] = []
+    var indexEn = 0
+    var indexSpa = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +34,9 @@ class GameViewController: UIViewController {
     }
     
     func fetchData()  {
-        
         var arrayOfWords = [Word]()
         
-        guard let url = URL(string: "https://gist.githubusercontent.com/DroidCoder/7ac6cdb4bf5e032f4c737aaafe659b33/raw/baa9fe0d586082d85db71f346e2b039c580c5804/words.json") else {return}
+        guard let url = URL(string: "https://gist.githubusercontent.com/DroidCoder/7ac6cdb4bf5e032f4c737aaafe659b33/raw/baa9fe0d586082d85db71f346e2b039c580c5804/words.json") else { return }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let dataResponse = data,
@@ -50,17 +51,53 @@ class GameViewController: UIViewController {
                     self.vocabularyEn.append(i.textEn)
                     self.vocabularySpa.append(i.textSpa)
                 }
-                
                 DispatchQueue.main.async {
-                    self.languageOne.text = self.vocabularyEn[0]
-                    self.languageTwo.text = self.vocabularySpa[0]
+                    self.indexSpa = Int(arc4random_uniform(UInt32(self.vocabularySpa.count)))
+                    self.indexEn = 0
+                    self.languageOne.text = self.vocabularyEn[self.indexEn]
+                    self.languageTwo.text = self.vocabularySpa[self.indexSpa]
                 }
-                
             } catch let parsingError {
                 print("Error", parsingError)
             }
         }
         task.resume()
+    }
+    
+    func checkAnswer() {
+        if indexEn == indexSpa {
+            print("IS CORRECT!!!")
+        } else {
+            print("IS WRONG :(")
+        }
+    }
+    
+    func showNextWord() {
+        if indexEn < vocabularyEn.count - 1 {
+            let randomNumber = Int.random(in: 0 ... 500) // Avoids to create a pattern presenting the matching answers.
+            indexEn += 1
+            
+            if randomNumber % 3 == 0 {
+                indexSpa = Int(arc4random_uniform(UInt32(self.vocabularySpa.count)))
+            } else {
+                indexSpa = indexEn
+            }
+            
+            languageOne.text = vocabularyEn[indexEn]
+            languageTwo.text = vocabularySpa[indexSpa]
+        } else {
+            print("GAME OVER")
+        }
+    }
+   
+    @IBAction func selectCorrect(_ sender: UIButton) {
+        checkAnswer()
+        showNextWord()
+    }
+    
+    @IBAction func selectWrong(_ sender: UIButton) {
+        checkAnswer()
+        showNextWord()
     }
     
 }
